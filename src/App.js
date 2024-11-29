68aeb5bd821459b5ee2e385023f0b4fc2ed277a4
Eun Ji <kws03337@naver.com>
2024-11-29 17:51:16 +0900

    241129 리액트 연습

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+/*eslint-disable*/
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [글제목, set글제목] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬독학']);
+  let [like, setLike] = useState(0);
+  let [modal, setModal] = useState(false);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "black-nav">
+        <h4>ReactBlog</h4>
+      </div>
+      <div className = "list">
+        <h4>{글제목[0]} <span onClick={()=>{setLike(like + 1)}}>👍</span> {like} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{글제목[1]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div className = "list">
+        <h4>{글제목[2]}</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <Modal/>
     </div>
   );
+}
+function Modal(){
+  return(
+    <div className='modal'>
+        <h4>제목</h4>
+        <p>날짜</p>
+        <p>상세내용</p>
+      </div>
+  )
 }
 
 export default App;
