da528ada7a56a33d793533479716c028674d84e8
Eun Ji <kws03337@naver.com>
2024-11-29 18:09:22 +0900

    241129

--- src/App.js
+++ src/App.js
@@ -6,16 +6,15 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, set글제목] = useState(['남자 코드 추천', '강남 우동 맛집', '파이썬독학']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(false);
   
-
   return (
     <div className="App">
       <div className = "black-nav">
         <h4>ReactBlog</h4>
       </div>
-      <div className = "list">
+      {/* <div className = "list">
         <h4>{글제목[0]} <span onClick={()=>{setLike(like + 1)}}>👍</span> {like} </h4>
         <p>2월 17일 발행</p>
       </div>
@@ -24,10 +23,22 @@ function App() {
         <p>2월 17일 발행</p>
       </div>
       <div className = "list">
-        <h4>{글제목[2]}</h4>
+        <h4 onClick={()=>{modal==true? setModal(false) : setModal(true)}}>{글제목[2]}</h4>
         <p>2월 17일 발행</p>
-      </div>
-      <Modal/>
+      </div> */}
+      {
+        글제목.map(function(a, i){
+          return(
+            <div className = "list" key={i}>
+            <h4 onClick={()=>{modal==true? setModal(false) : setModal(true)}}>{글제목[i]}<span onClick={()=>{setLike[i](like[i] + 1)}}>👍</span> {like[i]} </h4>
+            <p>2월 17일 발행</p>
+          </div>
+          )
+        })
+      }
+      {
+        modal==true ? <Modal></Modal> : null 
+      }
     </div>
   );
 }
